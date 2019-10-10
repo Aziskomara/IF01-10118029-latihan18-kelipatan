16865ba052e7d42a2afba6598b4b8db0f210aa7c
Aziskomara <55784203+Aziskomara@users.noreply.github.com>
2019-10-10 16:25:07 +0700

    Add files via upload

---
+++ IF01-10118029-latihan18-kelipatan/src/latihan18/Latihan18.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan18;
+
+/**
+ *
+ * @author acer
+ * NAMA      : Azis komara
+ * KELAS     : IF-1
+ * NIM       : 10118029
+ * Deskripsi Program   : Program ini berisi program kelipatan
+ */
+public class Latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //validasi
+       double i = 3.5;
+       while(i <= 35){
+           System.out.println(i);
+           i += 3.5;
+       }
+       
+       
+    }
+    
+}
